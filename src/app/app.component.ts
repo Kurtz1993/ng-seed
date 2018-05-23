a77799cfcff199588c6da5074c4438163c2332fe
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-05-23 14:02:38 -0500

    Remove unused lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-import { BaseService, LogService } from 'app/core/services';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-  constructor(private logService: LogService) {}
-
-  ngOnInit(): void {
-    this.logService.logError(new Error('suck my deek')).subscribe(() => {});
-  }
+export class AppComponent {
+  constructor() {}
 }
