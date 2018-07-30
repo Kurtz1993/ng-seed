5ea03caaa597e2fcf71bad8a1d7861cb19180234
Luis Hernandez <luis_hernandez@epam.com>
2018-07-30 12:03:23 -0500

    Remove LogLevel enum

--- src/app/shared/enums/index.ts
+++ src/app/shared/enums/index.ts
@@ -1,4 +1,3 @@
 // Use this file to export all your enums.
 // Then they can be imported on other files like this. E.g.
 // import { AuthenticationStatus } from 'app/shared/enums';
-export * from './log-level.enum';
